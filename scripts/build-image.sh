5b7efeb6ae2c75b9e351c5cf6837600b11b5b0c8
darsh2507 <64639571+darsh2507@users.noreply.github.com>
2024-09-26 22:11:48 +1000

    Update build-image.sh

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -1,1 +1,10 @@
 # Your Solution
+set -u # or set -o nounset
+: "$CONTAINER_REGISTRY"
+: "$VERSION"
+
+docker compose -f ./docker-compose.yml build
+
+docker tag books $CONTAINER_REGISTRY/bookf:latest 
+
+docker tag inventory $CONTAINER_REGISTRY/inventorf:latest
