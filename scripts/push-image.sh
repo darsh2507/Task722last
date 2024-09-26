cac7fb542921e95406b893afc3f75dc4f82dfbfb
darsh2507 <64639571+darsh2507@users.noreply.github.com>
2024-09-26 22:14:15 +1000

    Update push-image.sh

--- scripts/push-image.sh
+++ scripts/push-image.sh
@@ -1,1 +1,12 @@
 # Your Solution
+set -u # or set -o nounset
+: "$CONTAINER_REGISTRY"
+: "$VERSION"
+: "$REGISTRY_UN"
+: "$REGISTRY_PW"
+
+echo $REGISTRY_PW | docker login $CONTAINER_REGISTRY --username $REGISTRY_UN --password-stdin
+
+docker push $CONTAINER_REGISTRY/bookf:latest
+
+docker push $CONTAINER_REGISTRY/inventoryf:latest
